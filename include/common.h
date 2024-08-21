02b7cf21e6f9706f8028e4c3b0a3dbb7c4dd188b
Clyde Mar <clydemar234@gmail.com>
2024-08-21 13:34:05 +0800

    feat(config): Add include/common.h used for referencing global constants and types

---
+++ include/common.h
@@ -0,0 +1,21 @@
+
+#ifndef COMMON_H
+#define COMMON_H
+
+
+#define WINDOW_WIDTH    1200
+#define WINDOW_HEIGHT   600
+
+typedef struct {
+    int x;
+    int y;
+} VectorInt2;
+
+typedef struct {
+    float x;
+    float y;
+} VectorFloat2;
+
+#endif
+
+
