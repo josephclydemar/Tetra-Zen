ae181a2529c130b7d2afaa58b0c69a6ae75fb197
Clyde Mar <clydemar234@gmail.com>
2024-08-24 22:41:48 +0800

    feat(main/brick): Add periodic 1 unit falling of bricks

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 #include "raylib.h"
 #include "queue.h"
@@ -8,25 +9,31 @@
 #include "brick.h"
 
 int main(void) {
-    srand(time(0));
+    //srand(time(0));
+
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Tetra Zen");
     SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
 
     Brick *myBricks[7] = {
-        BrickCreate(I_BRICK, 25, 3, RED),
-        BrickCreate(T_BRICK, 3, 5, YELLOW),
-        BrickCreate(O_BRICK, 7, 7, GREEN),
-        BrickCreate(J_BRICK, 12, 6, BLUE),
-        BrickCreate(L_BRICK, 14, 5, ORANGE),
-        BrickCreate(S_BRICK, 17, 17, MAROON),
-        BrickCreate(Z_BRICK, 25, 13, VIOLET)
+        BrickCreate(I_BRICK, 0, 6, 2, RED),
+        BrickCreate(L_BRICK, 0, 11,  7, YELLOW),
+        BrickCreate(J_BRICK, 0, 16, 10, GREEN),
+        BrickCreate(T_BRICK, 0, 21, 13, BLUE),
+        BrickCreate(O_BRICK, 0, 26, 15, ORANGE),
+        BrickCreate(S_BRICK, 0, 31, 18, PINK),
+        BrickCreate(Z_BRICK, 0, 36, 22, VIOLET)
     };
-
+    clock_t timeInterval = clock() + CLOCKS_PER_SEC;
 
     // Main game loop
     while(!WindowShouldClose()) {       // Detect window close button or ESC key
-
         //----------------------------------------------------------------------------------
+        if(clock() > timeInterval) {
+            for(int i = 0; i < 7; i++) {
+                BrickFall(myBricks[i]);
+            }
+            timeInterval = clock() + CLOCKS_PER_SEC;
+        }
         BeginDrawing();
         ClearBackground(BLACK);
         for(int i = 0; i < 7; i++) {
