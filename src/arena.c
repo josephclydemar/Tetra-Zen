5748807317ffc03986772cd12a5edeb12bbc8e09
Clyde Mar <clydemar234@gmail.com>
2024-09-03 18:30:09 +0800

    fix(arena): Initialize active Brick falling speed at creation of Arena

--- src/arena.c
+++ src/arena.c
@@ -9,6 +9,7 @@
 Arena *CreateArena(void) {
     Arena *newArena = (Arena*)malloc(sizeof(Arena));
     newArena->activeBrick = NULL;
+    newArena->brickFallSpeed = 1;
     newArena->landedBlocks = CreateLList();
     newArena->completeLineBlocks = CreateStack();
     return newArena;
