4d28447b2de9daa99185e067f8637360722e5c9a
Clyde Mar <clydemar234@gmail.com>
2024-09-09 15:46:14 +0800

    refactor: Rename Arena to Game

---
+++ include/game.h
@@ -0,0 +1,25 @@
+#include <time.h>
+
+#include "llist.h"
+#include "stack.h"
+
+#ifndef GAME_H
+#define GAME_H
+
+
+typedef struct {
+    void *activeBrick;
+    clock_t brickFallSpeed;
+    LList *landedBlocks;
+    Stack *completeLineBlocks;
+} Game;
+
+
+Game *GameCreate(void);
+void GameActiveBrickLand(Game *game);
+void GameActiveBrickCollide(Game *game);
+clock_t GameUpdate(Game *game, clock_t timeInterval);
+
+
+
+#endif
