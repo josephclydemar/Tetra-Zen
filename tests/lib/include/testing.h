0ff1936802784f5f860dd710d3041092105d1491
Clyde Mar <clydemar234@gmail.com>
2024-09-14 00:58:46 +0800

    refactor: Put console color escape sequences as macros

--- tests/lib/include/testing.h
+++ tests/lib/include/testing.h
@@ -6,28 +6,41 @@
 #include <stdbool.h>
 
 
-// #define BLACK_FORE      "\0[30m"
-// #define BLACK_BACK      "\0[40m"
+#define BLACK_FORE      "\033[30m"
+#define BLACK_BACK      "\033[40m"
 
-// #define RED_FORE        "\0[31m"
-// #define RED_BACK        "\0[41m"
 
-// #define GREEN_FORE      "\0[32m"
-// #define GREEN_BACK      "\0[42m"
+#define RED_FORE        "\033[31m"
+#define RED_BACK        "\033[41m"
 
-// #define YELLOW_FORE	    "\0[33m"
-// #define YELLOW_BACK	    "\0[43m"
+#define LIGHTRED_FORE   "\033[91m"
+#define LIGHTRED_BACK   "\033[101m"
 
-// #define BLUE_FORE       "\0[34m"
-// #define BLUE_BACK       "\0[44m"
 
-// #define MAGENTA_FORE	"\0[35m"
-// #define MAGENTA_BACK	"\0[45m"
+#define GREEN_FORE      "\033[32m"
+#define GREEN_BACK      "\033[42m"
 
-// #define CYAN_FORE       "\0[36m"
-// #define CYAN_BACK       "\0[46m"
+#define LIGHTGREEN_FORE "\033[92m"
+#define LIGHTGREEN_BACK "\033[102m"
 
-// #define DEFAULT_FORE	"\0[0m"
+
+#define YELLOW_FORE	    "\033[33m"
+#define YELLOW_BACK	    "\033[43m"
+
+
+#define BLUE_FORE       "\033[34m"
+#define BLUE_BACK       "\033[44m"
+
+
+#define MAGENTA_FORE	"\033[35m"
+#define MAGENTA_BACK	"\033[45m"
+
+
+#define CYAN_FORE       "\033[36m"
+#define CYAN_BACK       "\033[46m"
+
+
+#define DEFAULT_FORE	"\033[0m"
 
 
 bool Test(const char *_func_name, const bool _condition, const char *_description);
--- tests/lib/src/testing.c
+++ tests/lib/src/testing.c
@@ -2,11 +2,24 @@
 #include "testing.h"
 
 bool Test(const char *_func_name, bool _condition, const char *_description) {
-    if(_condition == false) {
-        printf("    \033[0m[\033[31mFAILED\033[0m] \033[33m%s\033[0m : %s\n", _func_name, _description);
-        return false;
+    if(_condition) {
+        printf("    %s%s PASSED %s %s%s%s : %s\n",
+            LIGHTGREEN_BACK, 
+            BLACK_FORE, 
+            DEFAULT_FORE, 
+            YELLOW_FORE, 
+            _func_name, 
+            DEFAULT_FORE, 
+            _description);
+        return true;
     }
-    printf("    \033[0m[\033[32mPASSED\033[0m] \033[33m%s\033[0m : %s\n", _func_name, _description);
-    return true;
+    printf("    %s%s FAILED %s %s%s%s : %s\n", 
+        RED_BACK, 
+        BLACK_FORE, 
+        DEFAULT_FORE, 
+        YELLOW_FORE, 
+        _func_name, 
+        DEFAULT_FORE, 
+        _description);
+    return false;
 }
-
